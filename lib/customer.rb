464d9e15f83285afbed1c90b396122bc03b05e2d
D-Justice <justice.damian96@gmail.com>
2021-11-10 06:01:02 +1100

    Assignment Complete

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
-  
+    attr_accessor :meal, :age
+    @@all = []
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(waiter, price, tip)
+        meal = Meal.new(waiter, self, price, tip)
+    end
+    def meals
+        Meal.all.select {|meal| meal.customer == self}
+    end
+    def waiters
+        Meal.all.select {|meal| meal.customer == self}.collect {|each| each.waiter}
+    end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
+    attr_reader :name, :experience
+    @@all = []
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        Meal.all.select {|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        sorted = Meal.all.sort {|a, b| b.tip <=> a.tip}
+        sorted.first.customer
+    end
 end
